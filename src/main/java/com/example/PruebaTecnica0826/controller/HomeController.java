6f78117ddc31168d05f2536cf87dcaa82cc8dc2b
Emerson1911 <1284662@clases.edu.sv>
2024-08-26 10:15:46 -0600

    Commit

---
+++ src/main/java/com/example/PruebaTecnica0826/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.PruebaTecnica0826.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping 
+    public String home() {
+            return "index";
+    }
+}
---
+++ src/test/java/com/example/PruebaTecnica0826/PruebaTecnica0826ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PruebaTecnica0826;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnica0826ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
